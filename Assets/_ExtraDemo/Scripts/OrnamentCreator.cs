cfaba3f330984f50e37a59bffb1a307ca8ed27d5
a_hancho-desktop <atsuhan220@gmail.com>
2020-09-28 13:45:36 +0900

    feat : add ARWorldMapWithAnchorAndOrnament demo

---
+++ Assets/_ExtraDemo/Scripts/OrnamentCreator.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARAnchorManager))]
+[RequireComponent(typeof(ARRaycastManager))]
+public class OrnamentCreator : MonoBehaviour
+{
+    [SerializeField] private GameObject _ornamentPrefab;
+    [SerializeField] private Transform _anchorTrackingTransform;
+
+    private GameObject _ornamentObj;
+    private ARRaycastManager _arRaycastManager;
+    private static List<ARRaycastHit> _raycastHits = new List<ARRaycastHit>();
+
+    void Start()
+    {
+        _arRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        // 2本タップ以外はスルー
+        if (Input.touchCount != 2)
+        {
+            return;
+        }
+        
+        // uGUI操作時は無視
+        if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId)) {
+            return;
+        }
+
+        // タップ開始時以外はスルー
+        Touch touch = Input.GetTouch(0);
+        if (touch.phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+        // タップ処理
+        if (_arRaycastManager.Raycast(touch.position, _raycastHits, TrackableType.FeaturePoint))
+        {
+            Pose hitPose = _raycastHits[0].pose;
+            UpdateOrnament(hitPose);
+        }
+    }
+
+    /// <summary>
+    /// 置物の作成・位置の更新
+    /// </summary>
+    /// <param name="pose">配置位置</param>
+    private void UpdateOrnament(Pose pose, bool isLocalPose = false)
+    {
+        if (_ornamentObj == null)
+        {
+            _ornamentObj = Instantiate(_ornamentPrefab, _anchorTrackingTransform);
+        }
+
+        if (isLocalPose)
+        {
+            _ornamentObj.transform.localPosition = pose.position;
+            _ornamentObj.transform.localRotation = pose.rotation;
+        }
+        else
+        {
+            _ornamentObj.transform.SetPositionAndRotation(pose.position, pose.rotation);
+        }
+    }
+    
+    /// <summary>
+    /// アンカーに対する置物の位置を保存
+    /// </summary>
+    public void SaveOrnamentPoseFromAnchor()
+    {
+        // 置物がないときはスルー
+        if (_ornamentObj == null)
+        {
+            Debug.LogError("ARAnchor or ornament is not found;");
+            return;
+        }
+
+        // Anchorとの位置関係を保存
+        Vector3 diffPos = _ornamentObj.transform.localPosition;
+        Quaternion diffRot =  _ornamentObj.transform.localRotation;
+        PlayerPrefs.SetFloat("OrnamentPosX", diffPos.x);
+        PlayerPrefs.SetFloat("OrnamentPosY", diffPos.y);
+        PlayerPrefs.SetFloat("OrnamentPosZ", diffPos.z);
+        PlayerPrefs.SetFloat("OrnamentRotX", diffRot.x);
+        PlayerPrefs.SetFloat("OrnamentRotY", diffRot.y);
+        PlayerPrefs.SetFloat("OrnamentRotZ", diffRot.z);
+        PlayerPrefs.SetFloat("OrnamentRotW", diffRot.w);
+    }
+
+    /// <summary>
+    /// アンカーに対する置物の位置を読み込んで移動
+    /// </summary>
+    public void LoadOrnamentPoseFromAnchor()
+    {
+        // Anchorとの位置関係を読み込み
+        float posX = PlayerPrefs.GetFloat("OrnamentPosX");
+        float posY = PlayerPrefs.GetFloat("OrnamentPosY");
+        float posZ = PlayerPrefs.GetFloat("OrnamentPosZ");
+        float rotX = PlayerPrefs.GetFloat("OrnamentRotX");
+        float rotY = PlayerPrefs.GetFloat("OrnamentRotY");
+        float rotZ = PlayerPrefs.GetFloat("OrnamentRotZ");
+        float rotW = PlayerPrefs.GetFloat("OrnamentRotW");
+        Vector3 diffPos = new Vector3(posX, posY, posZ);
+        Quaternion diffRot = new Quaternion(rotX, rotY, rotZ, rotW);
+        Pose diffPose = new Pose(diffPos, diffRot);
+
+        // 置物移動
+        UpdateOrnament(diffPose, true);
+    }
+}
---
+++ Assets/_ExtraDemo/Scripts/SingleAnchorCreator.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARAnchorManager))]
+[RequireComponent(typeof(ARRaycastManager))]
+public class SingleAnchorCreator : MonoBehaviour
+{
+    [SerializeField] private Transform _anchorTrackingTransform;
+    
+    private ARAnchor _anchor;
+    private ARRaycastManager _raycastManager;
+    private ARAnchorManager _anchorManager;
+    private static List<ARRaycastHit> _raycastHits = new List<ARRaycastHit>();
+    
+    public ARAnchor Anchor => _anchor;
+
+    void Awake()
+    {
+        _raycastManager = GetComponent<ARRaycastManager>();
+        _anchorManager = GetComponent<ARAnchorManager>();
+
+        _anchorManager.anchorsChanged += OnAnchorsChanged;
+    }
+
+    void Update()
+    {
+        // シングルタップ時以外は無視
+        if (Input.touchCount != 1)
+        {
+            return;
+        }
+
+        // uGUI操作時は無視
+        if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+        {
+            return;
+        }
+
+        // タッチ開始時以外は無視
+        Touch touch = Input.GetTouch(0);
+        if (touch.phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+
+        if (_raycastManager.Raycast(touch.position, _raycastHits, TrackableType.FeaturePoint))
+        {
+            // 古いAnchor削除
+            RemoveAnchor();
+            
+            // 新規Anchor作成
+            Pose hitPose = _raycastHits[0].pose;
+            AddAnchor(hitPose);
+        }
+    }
+    
+    private void OnAnchorsChanged(ARAnchorsChangedEventArgs eventArgs)
+    {
+        if (eventArgs.added.Count > 0)
+        {
+            Transform targetTransform = eventArgs.added.Last().transform;
+            Pose targetPose = new Pose(targetTransform.position, targetTransform.rotation);
+            MoveTrackingObj(targetPose);
+        }
+    }
+    
+    public void AddAnchor(Pose hitPose)
+    {
+        _anchor = _anchorManager.AddAnchor(hitPose);
+        if (_anchor == null)
+        {
+            Logger.Log("Error creating anchor");
+        }
+    }
+
+    public void RemoveAnchor()
+    {
+        if (_anchor == null)
+        {
+            return;
+        }
+        
+        _anchorManager.RemoveAnchor(_anchor);
+        _anchor = null;
+    }
+    
+    public void MoveTrackingObj(Pose pose)
+    {
+        _anchorTrackingTransform.SetPositionAndRotation(pose.position, pose.rotation);
+    }
+    
+}
